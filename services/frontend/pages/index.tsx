d425f445c9e6ecd00de703c785aa3683f5707de2
Correia Alves Cedric <cedric.correia.alves@gmail.com>
2020-01-28 01:49:57 +0100

    add some test pages to service frontend

---
+++ services/frontend/pages/index.tsx
@@ -0,0 +1,8 @@
+export default () => {
+  return (
+    <>
+      <h1>hello</h1>
+      <p>this will be the landing page</p>
+    </>
+  );
+};
---
+++ services/frontend/pages/test.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <h1>Test</h1>;
+};
